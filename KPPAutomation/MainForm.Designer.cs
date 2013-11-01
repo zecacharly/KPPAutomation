5a2735b35c0ba2fcd60bf0ed67e55763edb6e0c8
Rui Sebastiao <ruisebastiao@gmail.com>
2013-11-01 13:49:33 +0000

    Revision 1.1

--- KPPAutomation/MainForm.Designer.cs
+++ KPPAutomation/MainForm.Designer.cs
@@ -23,20 +23,237 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.@__MenuBar = new System.Windows.Forms.ToolStrip();
+            this.toolStripSeparator9 = new System.Windows.Forms.ToolStripSeparator();
+            this.@__dropLogin = new System.Windows.Forms.ToolStripDropDownButton();
+            this.toolStripMenuItem13 = new System.Windows.Forms.ToolStripMenuItem();
+            this.@__logpass = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator10 = new System.Windows.Forms.ToolStripSeparator();
+            this.@__btlogin = new System.Windows.Forms.ToolStripMenuItem();
+            this.changePasswordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.@__editnewpass = new System.Windows.Forms.ToolStripTextBox();
+            this.mostrarPasswordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.confirmarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addUserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.userNameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.@__editUsername = new System.Windows.Forms.ToolStripTextBox();
+            this.passwordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.@__editnewuserpass = new System.Windows.Forms.ToolStripTextBox();
+            this.levelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.@__toolStriplevels = new System.Windows.Forms.ToolStripComboBox();
+            this.confirmarToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator11 = new System.Windows.Forms.ToolStripSeparator();
+            this.@__btLanguage = new System.Windows.Forms.ToolStripDropDownButton();
+            this.englishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.portugueseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.@__btConfig = new System.Windows.Forms.ToolStripMenuItem();
+            this.@__MenuBar.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // __MenuBar
+            // 
+            resources.ApplyResources(this.@__MenuBar, "__MenuBar");
+            this.@__MenuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.@__btConfig,
+            this.toolStripSeparator9,
+            this.@__dropLogin,
+            this.toolStripSeparator11,
+            this.@__btLanguage});
+            this.@__MenuBar.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.@__MenuBar.Name = "__MenuBar";
+            this.@__MenuBar.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            // 
+            // toolStripSeparator9
+            // 
+            this.toolStripSeparator9.Name = "toolStripSeparator9";
+            resources.ApplyResources(this.toolStripSeparator9, "toolStripSeparator9");
+            // 
+            // __dropLogin
+            // 
+            this.@__dropLogin.BackColor = System.Drawing.Color.LightCoral;
+            this.@__dropLogin.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem13,
+            this.@__logpass,
+            this.toolStripSeparator10,
+            this.@__btlogin,
+            this.changePasswordToolStripMenuItem,
+            this.addUserToolStripMenuItem});
+            resources.ApplyResources(this.@__dropLogin, "__dropLogin");
+            this.@__dropLogin.Name = "__dropLogin";
+            this.@__dropLogin.ShowDropDownArrow = false;
+            // 
+            // toolStripMenuItem13
+            // 
+            this.toolStripMenuItem13.Name = "toolStripMenuItem13";
+            resources.ApplyResources(this.toolStripMenuItem13, "toolStripMenuItem13");
+            // 
+            // __logpass
+            // 
+            this.@__logpass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.@__logpass.Name = "__logpass";
+            resources.ApplyResources(this.@__logpass, "__logpass");
+            // 
+            // toolStripSeparator10
+            // 
+            this.toolStripSeparator10.Name = "toolStripSeparator10";
+            resources.ApplyResources(this.toolStripSeparator10, "toolStripSeparator10");
+            // 
+            // __btlogin
+            // 
+            this.@__btlogin.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.@__btlogin.Name = "__btlogin";
+            resources.ApplyResources(this.@__btlogin, "__btlogin");
+            // 
+            // changePasswordToolStripMenuItem
+            // 
+            this.changePasswordToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.@__editnewpass,
+            this.mostrarPasswordToolStripMenuItem,
+            this.confirmarToolStripMenuItem});
+            this.changePasswordToolStripMenuItem.Name = "changePasswordToolStripMenuItem";
+            resources.ApplyResources(this.changePasswordToolStripMenuItem, "changePasswordToolStripMenuItem");
+            // 
+            // __editnewpass
+            // 
+            this.@__editnewpass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.@__editnewpass.Name = "__editnewpass";
+            resources.ApplyResources(this.@__editnewpass, "__editnewpass");
+            // 
+            // mostrarPasswordToolStripMenuItem
+            // 
+            this.mostrarPasswordToolStripMenuItem.CheckOnClick = true;
+            this.mostrarPasswordToolStripMenuItem.Name = "mostrarPasswordToolStripMenuItem";
+            resources.ApplyResources(this.mostrarPasswordToolStripMenuItem, "mostrarPasswordToolStripMenuItem");
+            // 
+            // confirmarToolStripMenuItem
+            // 
+            this.confirmarToolStripMenuItem.Name = "confirmarToolStripMenuItem";
+            resources.ApplyResources(this.confirmarToolStripMenuItem, "confirmarToolStripMenuItem");
+            // 
+            // addUserToolStripMenuItem
+            // 
+            this.addUserToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.userNameToolStripMenuItem,
+            this.passwordToolStripMenuItem,
+            this.levelToolStripMenuItem,
+            this.confirmarToolStripMenuItem1});
+            this.addUserToolStripMenuItem.Name = "addUserToolStripMenuItem";
+            resources.ApplyResources(this.addUserToolStripMenuItem, "addUserToolStripMenuItem");
+            // 
+            // userNameToolStripMenuItem
+            // 
+            this.userNameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.@__editUsername});
+            this.userNameToolStripMenuItem.Name = "userNameToolStripMenuItem";
+            resources.ApplyResources(this.userNameToolStripMenuItem, "userNameToolStripMenuItem");
+            // 
+            // __editUsername
+            // 
+            this.@__editUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.@__editUsername.Name = "__editUsername";
+            resources.ApplyResources(this.@__editUsername, "__editUsername");
+            // 
+            // passwordToolStripMenuItem
+            // 
+            this.passwordToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.@__editnewuserpass});
+            this.passwordToolStripMenuItem.Name = "passwordToolStripMenuItem";
+            resources.ApplyResources(this.passwordToolStripMenuItem, "passwordToolStripMenuItem");
+            // 
+            // __editnewuserpass
+            // 
+            this.@__editnewuserpass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.@__editnewuserpass.Name = "__editnewuserpass";
+            resources.ApplyResources(this.@__editnewuserpass, "__editnewuserpass");
+            // 
+            // levelToolStripMenuItem
+            // 
+            this.levelToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.@__toolStriplevels});
+            this.levelToolStripMenuItem.Name = "levelToolStripMenuItem";
+            resources.ApplyResources(this.levelToolStripMenuItem, "levelToolStripMenuItem");
+            // 
+            // __toolStriplevels
+            // 
+            this.@__toolStriplevels.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.@__toolStriplevels.Name = "__toolStriplevels";
+            resources.ApplyResources(this.@__toolStriplevels, "__toolStriplevels");
+            // 
+            // confirmarToolStripMenuItem1
+            // 
+            this.confirmarToolStripMenuItem1.Name = "confirmarToolStripMenuItem1";
+            resources.ApplyResources(this.confirmarToolStripMenuItem1, "confirmarToolStripMenuItem1");
+            // 
+            // toolStripSeparator11
+            // 
+            this.toolStripSeparator11.Name = "toolStripSeparator11";
+            resources.ApplyResources(this.toolStripSeparator11, "toolStripSeparator11");
+            // 
+            // __btLanguage
+            // 
+            this.@__btLanguage.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.englishToolStripMenuItem,
+            this.portugueseToolStripMenuItem});
+            resources.ApplyResources(this.@__btLanguage, "__btLanguage");
+            this.@__btLanguage.Name = "__btLanguage";
+            // 
+            // englishToolStripMenuItem
+            // 
+            resources.ApplyResources(this.englishToolStripMenuItem, "englishToolStripMenuItem");
+            this.englishToolStripMenuItem.Name = "englishToolStripMenuItem";
+            // 
+            // portugueseToolStripMenuItem
+            // 
+            this.portugueseToolStripMenuItem.Checked = true;
+            this.portugueseToolStripMenuItem.CheckOnClick = true;
+            this.portugueseToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            resources.ApplyResources(this.portugueseToolStripMenuItem, "portugueseToolStripMenuItem");
+            this.portugueseToolStripMenuItem.Name = "portugueseToolStripMenuItem";
+            // 
+            // __btConfig
+            // 
+            resources.ApplyResources(this.@__btConfig, "__btConfig");
+            this.@__btConfig.Name = "__btConfig";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(744, 430);
+            this.Controls.Add(this.@__MenuBar);
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.@__MenuBar.ResumeLayout(false);
+            this.@__MenuBar.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip __MenuBar;
+        private System.Windows.Forms.ToolStripMenuItem __btConfig;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator9;
+        private System.Windows.Forms.ToolStripDropDownButton __dropLogin;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem13;
+        private System.Windows.Forms.ToolStripTextBox __logpass;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator10;
+        private System.Windows.Forms.ToolStripMenuItem __btlogin;
+        private System.Windows.Forms.ToolStripMenuItem changePasswordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox __editnewpass;
+        private System.Windows.Forms.ToolStripMenuItem mostrarPasswordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem confirmarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addUserToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem userNameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox __editUsername;
+        private System.Windows.Forms.ToolStripMenuItem passwordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox __editnewuserpass;
+        private System.Windows.Forms.ToolStripMenuItem levelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox __toolStriplevels;
+        private System.Windows.Forms.ToolStripMenuItem confirmarToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator11;
+        private System.Windows.Forms.ToolStripDropDownButton __btLanguage;
+        private System.Windows.Forms.ToolStripMenuItem englishToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem portugueseToolStripMenuItem;
     }
 }
 
